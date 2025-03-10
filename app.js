475c74d6c4460e16c714b84649062e2fd3d2ee47
Manlio <lt.lenin311@gmail.com>
2025-03-10 17:03:15 -0600

    add all javascript methods

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
-
+let listaAmigos=[]
 function agregarAmigo(){
-    console.log('amigo')
+
+    let amigo = document.getElementById("amigo").value;
+    if(amigo){
+        listaAmigos.push(amigo);
+        actualizarLista();
+        console.log(listaAmigos)
+        document.getElementById("amigo").value="";
+    }else{
+        alert("Debes ingresar el nombre del amigo");
+    }
+}
+function  actualizarLista(){
+    let lista = document.getElementById("listaAmigos");
+    lista.textContent="";
+
+    for(let i=0; i<listaAmigos.length; i++){
+        let li=document.createElement("li");
+        li.textContent=listaAmigos[i];
+        lista.appendChild(li);
+    }
+    console.log(listaAmigos)
+}
+function sortearAmigo(){
+    let aleatorio = Math.floor(Math.random() * listaAmigos.length);
+    alert(`El amigo seleccionado es: ${listaAmigos[aleatorio]} `)
+    listaAmigos.pop(listaAmigos[aleatorio])
+    console.log(listaAmigos)
 }
